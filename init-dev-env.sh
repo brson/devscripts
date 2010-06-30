c9a25a8b9306b7fa8ea88a585a80afc98367e5f6
Brian Anderson <andersrb@gmail.com>
2010-06-30 21:49:30 +0000

    Add more packages and move the sbt jar to the tools directory

--- init-dev-env.sh
+++ init-dev-env.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-PACKAGES="\
- git-core\
- emacs23-nox\
- bcrypt\
- git-svn\
+PACKAGES="
+ git-core
+ emacs23-nox
+ bcrypt
+ git-svn
  openjdk-6-jdk
  ant
  wget
  byobu
+ subversion
 "
 
-sudo aptitude install $PACKAGES
+sudo aptitude install -y $PACKAGES
 git config --global user.name "Brian Anderson"
 git config --global user.email "andersrb@gmail.com"
 
 HOME=/home/brian
 DEVDIR=$HOME/Dev
-mkdir $DEVDIR
-wget -O $DEVDIR/sbt-launch-0.7.4.jar http://simple-build-tool.googlecode.com/files/sbt-launch-0.7.4.jar
+mkdir -p $DEVDIR/tools
+wget -O $DEVDIR/tools/sbt-launch-0.7.4.jar http://simple-build-tool.googlecode.com/files/sbt-launch-0.7.4.jar
