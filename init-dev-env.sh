3c66cb092db2d5678cf407e2f06309d9a66725e3
Brian Anderson <andersrb@gmail.com>
2010-06-30 22:43:09 +0000

    Install Android 2.2 SDK

--- init-dev-env.sh
+++ init-dev-env.sh
@@ -11,6 +11,7 @@ PACKAGES="
  byobu
  subversion
  ia32-libs
+ unzip
 "
 
 sudo aptitude install -y $PACKAGES
@@ -27,3 +28,10 @@ wget -O $DEVDIR/tools/sbt-launch-0.7.4.jar http://simple-build-tool.googlecode.c
 
 wget -O $DEVDIR/downloads/android-sdk_r06-linux_86.tgz http://dl.google.com/android/android-sdk_r06-linux_86.tgz
 tar xzf $DEVDIR/downloads/android-sdk_r06-linux_86.tgz -C $DEVDIR
+wget -O $DEVDIR/downloads/android-2.1_r02-linux.zip http://dl-ssl.google.com/android/repository/android-2.1_r02-linux.zip
+unzip $DEVDIR/downloads/android-2.1_r02-linux.zip -d $DEVDIR/android-sdk-linux_86/platforms
+mv $DEVDIR/android-sdk-linux_86/platforms/android-2.1_r02-linux $DEVDIR/android-sdk-linux_86/platforms/android-7
+wget -O $DEVDIR/downloads/android-2.2_r01-linux.zip http://dl-ssl.google.com/android/repository/android-2.2_r01-linux.zip
+unzip $DEVDIR/downloads/android-2.2_r01-linux.zip -d $DEVDIR/android-sdk-linux_86/platforms
+mv $DEVDIR/android-sdk-linux_86/platforms/android-2.2_r01-linux $DEVDIR/android-sdk-linux_86/platforms/android-8
+
