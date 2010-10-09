25450d67385c0c1966a34e900259d85d4bed263a
Brian Anderson <andersrb@gmail.com>
2010-10-09 00:20:06 -0400

    Add a script for checking out scala-mode

---
+++ mirror-scala-mode.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+HOME=/home/brian
+DEVDIR=$HOME/Dev
+
+SCALA_MODE_DIR=$DEVDIR/scala-mode
+
+git svn clone http://lampsvn.epfl.ch/svn-repos/scala/scala-tool-support/trunk/src/emacs $SCALA_MODE_DIR
