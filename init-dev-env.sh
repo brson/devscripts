94ef00dbdc6968d38346c94bb401f922b605cb4d
Brian Anderson <andersrb@gmail.com>
2010-10-11 19:01:35 -0400

    Switch to android 2.2 r02

--- init-dev-env.sh
+++ init-dev-env.sh
@@ -19,7 +19,7 @@ tar xzf $DEVDIR/downloads/android-sdk_r06-linux_86.tgz -C $DEVDIR
 wget -O $DEVDIR/downloads/android-2.1_r02-linux.zip http://dl-ssl.google.com/android/repository/android-2.1_r02-linux.zip
 unzip $DEVDIR/downloads/android-2.1_r02-linux.zip -d $DEVDIR/android-sdk-linux_86/platforms
 mv $DEVDIR/android-sdk-linux_86/platforms/android-2.1_r02-linux $DEVDIR/android-sdk-linux_86/platforms/android-7
-wget -O $DEVDIR/downloads/android-2.2_r01-linux.zip http://dl-ssl.google.com/android/repository/android-2.2_r01-linux.zip
-unzip $DEVDIR/downloads/android-2.2_r01-linux.zip -d $DEVDIR/android-sdk-linux_86/platforms
-mv $DEVDIR/android-sdk-linux_86/platforms/android-2.2_r01-linux $DEVDIR/android-sdk-linux_86/platforms/android-8
+wget -O $DEVDIR/downloads/android-2.2_r02-linux.zip http://dl-ssl.google.com/android/repository/android-2.2_r02-linux.zip
+unzip $DEVDIR/downloads/android-2.2_r02-linux.zip -d $DEVDIR/android-sdk-linux_86/platforms
+mv $DEVDIR/android-sdk-linux_86/platforms/android-2.2_r02-linux $DEVDIR/android-sdk-linux_86/platforms/android-8
 
